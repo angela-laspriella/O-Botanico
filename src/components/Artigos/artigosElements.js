ded1f22473dcd19b4bbef6b6555891b940f58646
EdyS12 <eduardajsilva20@gmail.com>
2021-11-23 21:35:29 +0000

    Cartão1

--- src/components/Artigos/artigosElements.js
+++ src/components/Artigos/artigosElements.js
@@ -2,22 +2,58 @@ import styled from "styled-components";
 
 export const ArticlesContainer = styled.div`
   display: flex;
+  flex-direction: column;
   background: #fdfae3;
   height: 100vh;
   align-items: center;
-  margin-top: -80px;
+  /* margin-top: -80px; */
+  justify-content: center;
 `;
 
 export const TituloWrap = styled.div`
   width: 100%;
   max-width: 1200px;
-  border: red 1px solid;
+  margin-top: 50px;
+  /* border: red 1px solid; */
 `;
 
 export const ArtigosH1 = styled.h1`
-  position: relative;
   color: #f46036;
-
+  text-align: left;
   font-weight: 800;
-  font-size: 100px;
+  font-size: 90px;
+`;
+
+export const ThumbnailContainer = styled.div`
+  width: 90%;
+  padding-top: 64px;
+`;
+
+export const Artigo = styled.div`
+  display: grid;
+  height: 416px;
+  width: 360px;
+  border: black 2px solid;
+  justify-content: center;
+  text-align: left;
+  padding: 14px 14px 14px 14px;
+`;
+
+export const Headline = styled.h2`
+  color: black;
+  font-size: 20px;
+  margin-top: -10px;
+`;
+
+export const Subtitle = styled.h3`
+  color: black;
+  font-size: 14px;
+  font-family: "Lato";
+  margin-top: -15px;
+`;
+
+export const Text = styled.p`
+  color: black;
+  font-size: 13px;
+  font-family: "Lato";
 `;
--- src/components/Artigos/index.js
+++ src/components/Artigos/index.js
@@ -1,5 +1,15 @@
 import React from "react";
-import { ArticlesContainer, TituloWrap, ArtigosH1 } from "./artigosElements";
+import { ReactComponent as ImgArt } from "../../images/leslie.svg";
+import {
+  ArticlesContainer,
+  TituloWrap,
+  ArtigosH1,
+  ThumbnailContainer,
+  Artigo,
+  Headline,
+  Subtitle,
+  Text,
+} from "./artigosElements";
 
 const ArtigoSection = () => {
   return (
@@ -8,6 +18,22 @@ const ArtigoSection = () => {
         <TituloWrap>
           <ArtigosH1> Artigos </ArtigosH1>
         </TituloWrap>
+        <ThumbnailContainer>
+          <Artigo>
+            <ImgArt />
+            <Headline>Leslie causa destruição</Headline>
+            <Subtitle>13 de outubro de 2018</Subtitle>
+            <Text>
+              Um dos locais mais afetados pela tempestade Leslie foi o jardim
+              botânico da Universidade de Coimbra, onde se perderam algumas
+              árvores históricas. Vários voluntários e técnicos estão a fazer as
+              limpezas necessárias, mas antes do final de outubro o jardim não
+              deverá abrir portas. António Gouveia, diretor do jardim botânico,
+              revelou que o espaço foi “bastante afetado” e que, para além das
+              árvores, também muros e vedações ficaram danificados.
+            </Text>
+          </Artigo>
+        </ThumbnailContainer>
       </ArticlesContainer>
     </>
   );
