d3284b6d55dfaf8c505b972deb7b9dfca0f881e6
mrjgsantos <58551734+mrjgsantos@users.noreply.github.com>
2022-01-05 21:58:23 +0000

    firebaseeeee

--- src/components/Dashboard/EditTask.js
+++ src/components/Dashboard/EditTask.js
@@ -2,6 +2,7 @@ import Modal from "./Modal";
 import { useState } from "react";
 import "./editTask.css";
 import { doc, updateDoc } from "firebase/firestore";
+
 import { db } from "./firebase";
 
 function EditTask({ open, onClose, toEditTitle, toEditDate, toEditMonth, id }) {
